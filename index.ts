f71c09d1568e5f1db0bc26757caa910dd429be93
Guilhermo Masid <guilhermomasid@gmail.com>
2021-09-25 11:19:31 -0300

    chore: create user user post and user profile

--- index.ts
+++ index.ts
@@ -3,8 +3,26 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const users = await prisma.user.findMany();
-  console.log(users);
+  await prisma.user.create({
+    data: {
+      name: 'Guilhermo',
+      email: 'guilhermo@gmail.com',
+      posts: {
+        create: { title: 'First one' },
+      },
+      profile: {
+        create: { bio: 'Just a young developer' },
+      },
+    },
+  });
+
+  const users = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  });
+  console.dir(users, { depth: null });
 }
 
 main()
